40f62de086b57ab10cba5283088ac7ad66f58a70
shardul9611 <sharmasharduljind@gmail.com>
2024-11-22 11:38:36 +0530

    updated Files

---
+++ my-project/src/components/About.jsx
@@ -0,0 +1,73 @@
+import React from "react";
+
+function About() {
+  return (
+    <div
+      name="About"
+      className="max-w-screen-2xl container mx-auto px-4 md:px-20 my-20"
+    >
+      <div>
+        <h1 className="text-3xl font-bold mb-5">About</h1>
+        <p>
+          Hello, I'm Shardul, a passionate Web developer with a keen eye for MERN
+          Stack . With a background in IT, I strive to create impactful and
+          visually stunning Software solutions that leave a lasting impression.
+        </p>
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Education & Training
+        </h1>
+        <span>
+        I hold a B.Tech degree and a Post Graduate Diploma in Advanced Computing from [C-DAC] Pune, where I gained expertise in Full Stack Development, including HTML, CSS, JavaScript, Node.js, Express.js, MongoDB, and MySQL. This advanced training reflects my commitment to professional growth and equips me to tackle challenges and contribute effectively as a Full Stack Developer.
+        </span>
+        <br />
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Skills & Expertise
+        </h1>
+        <span>
+        I am a skilled full-stack developer specializing in building dynamic, responsive web applications. Proficient in front-end technologies like HTML, CSS, JavaScript, and React, I create intuitive user interfaces. My back-end expertise includes Node.js, Express.js, and API development, with strong proficiency in database management using MongoDB and MySQL. I am well-versed in version control with Git and deployment processes, ensuring efficient and scalable solutions.
+        </span>
+        <br />
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Professional Experience
+        </h1>
+        <span>
+        1. Full Stack Developer – Ambroasian Research and Development Pvt Ltd. Pune<br></br>
+         June-2023 – Feb-2024<br></br>
+          Developed scalable web applications using MERN Stack. Led API development, optimized performance, 
+          and collaborated with teams to deliver user-focused solutions.
+          <hr class="border-1 border-black my-1"></hr>
+          2. Full Stack Developer – 360 Bytes Tech Venture Private Limited, Mohali<br></br>
+          Feb-2024- Present<br></br>
+          Developed scalable web applications using MERN Stack. Led API development, optimized performance, 
+          and collaborated with teams to deliver user-focused solutions. Conducted code reviews and implemented best practices to ensure high-quality deliverables.
+        </span>
+        <br />
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Achievements & Awards
+        </h1>
+        <span>
+        Employee of the Month – Recognized for exceptional performance and teamwork in delivering a high-impact project within tight deadlines.<br />
+         Best Innovator Award – Awarded for implementing a feature that improved application efficiency by 50%.<br />
+         Certification of Excellence – Honored for consistent quality in full-stack development and client satisfaction.
+        </span>
+        <br />
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Mission Statement
+        </h1>
+        <p>
+        As a Full Stack Developer, my mission is to create dynamic, scalable, and user-friendly web applications that meet both business and user 
+        needs. I am dedicated to continuous learning and improvement, utilizing the latest technologies and best practices in both front-end 
+        and back-end development. By fostering collaboration and embracing innovation, I strive to deliver high-quality,
+         efficient solutions that drive business growth and enhance the user experience.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default About;
---
+++ my-project/src/components/Home.jsx
@@ -0,0 +1,96 @@
+import React from "react";
+
+import pic from "../../public/photo.avif";
+
+import { FaSquareFacebook } from "react-icons/fa6";
+import { FaLinkedin } from "react-icons/fa";
+import { IoLogoYoutube } from "react-icons/io";
+import { FaTelegram } from "react-icons/fa6";
+
+import { SiMongodb } from "react-icons/si";
+import { SiExpress } from "react-icons/si";
+import { FaReact } from "react-icons/fa";
+import { FaNodeJs } from "react-icons/fa6";
+
+import { ReactTyped } from "react-typed";
+
+function Home() {
+  return (
+    <>
+      <div
+        name="Home"
+        className="max-w-screen-2xl container mx-auto px-4 md:px-20 my-28"
+      >
+        <div className="flex flex-col md:flex-row">
+          <div className="md:w-1/2 mt-12 md:mt-24 space-y-2 order-2 md:order-1">
+            <span className="text-xl">Welcome In My Feed</span>
+            <div className="flex space-x-1 text-2xl md:text-4xl">
+              <h1>Hello, I'm a </h1>
+              {/* <span >Developer</span> */}
+              <ReactTyped
+                className="text-green-700 font-bold"
+                strings={["Developer", "Programmer", "Coder"]}
+                typeSpeed={40}
+                backSpeed={50}
+                loop={true}
+              />
+            </div>
+            <br />
+            <p className="text-sm md:text-md text-justify">
+            I’m a  Fullstack Developer with a strong foundation in both front-end and back-end technologies. I specialize in building responsive and user-friendly web applications using tools like HTML, CSS, JavaScript, and frameworks like React for the front-end. On the server-side, I work with Node.js, Express, and databases such as MongoDB or MySQL. Passionate about problem-solving and clean code, I’m constantly exploring new technologies to enhance my skills. With a focus on collaboration and continuous learning, I am excited to contribute to innovative projects and grow in a dynamic development environment
+            </p>
+            <br />
+            {/* social media icons */}
+            <div className="flex flex-col items-center md:flex-row justify-between space-y-6 md:space-y-0">
+              <div className="  space-y-2">
+                <h1 className="font-bold text-center ">Available on</h1>
+                <ul className="flex space-x-5">
+                  <li>
+                    <a href="https://www.facebook.com/" target="_blank">
+                      <FaSquareFacebook className="text-2xl cursor-pointer" />
+                    </a>
+                  </li>
+                  <li>
+                    <a href="https://www.linkedin.com/" target="_blank">
+                      <FaLinkedin className="text-2xl cursor-pointer" />
+                    </a>
+                  </li>
+                  <li>
+                    <a href="https://www.youtube.com/" target="_blank">
+                      <IoLogoYoutube className="text-2xl cursor-pointer" />
+                    </a>
+                  </li>
+                  <li>
+                    <a href="https://telegram.org/" target="_blank">
+                      <FaTelegram className="text-2xl cursor-pointer" />
+                    </a>
+                  </li>
+                </ul>
+              </div>
+              <div className=" space-y-2">
+                <h1 className="font-bold text-center">Currently working on</h1>
+                <div className="flex space-x-5">
+                  <SiMongodb className="text-2xl md:text-3xl hover:scale-110 duration-200 rounded-full border-[2px] cursor-pointer" />
+                  <SiExpress className="text-2xl md:text-3xl hover:scale-110 duration-200 rounded-full border-[2px] cursor-pointer" />
+                  <FaReact className="text-2xl md:text-3xl hover:scale-110 duration-200 rounded-full border-[2px] cursor-pointer" />
+                  <FaNodeJs className="text-2xl md:text-3xl hover:scale-110 duration-200 rounded-full border-[2px] cursor-pointer" />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="md:w-1/2 md:ml-48 md:mt-20 mt-8 order-1">
+            <img
+              src={pic}
+              className="rounded-full md:w-[450px] md:h-[450px]"
+              alt=""
+            />
+          </div>
+        </div>
+      </div>
+
+      <hr />
+    </>
+  );
+}
+
+export default Home;
